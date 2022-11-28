fac61592841c5766c758bff72001aeeb237091d7
sotopaguayj <sotopaguayj@gmail.com>
2022-11-27 22:29:06 -0500

    allReady

--- src/pages/api/getNews.js
+++ src/pages/api/getNews.js
@@ -1,7 +1,7 @@
 import Tmp from "models/Tmp";
 
 export default async function getNews(req, res) {
-  await Tmp.find({}).then((result) => {
+  await Tmp.find().then((result) => {
     res.status(200).json(result);
   });
 }
